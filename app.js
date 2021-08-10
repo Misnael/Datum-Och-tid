b658dbfac78e827d56bc7caa6870086dc427c8b4
Misnael <71625384+Misnael@users.noreply.github.com>
2021-08-10 13:37:03 +0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,30 @@
+const timeElement = document.getElementById("time")
+const dateElement = document.getElementById("date")
+
+setInterval(tick, 1)
+    
+
+function tick() {
+ 
+
+
+
+const date = new Date();
+
+timeElement.innerHTML =
+    pad(date.getHours(), 2)  + ":" +
+    pad(date.getMinutes(), 2)  + ":" +
+    pad(date.getSeconds(), 2) ;
+
+dateElement.innerHTML =
+    pad(date.getFullYear(), 2)  + ":" +
+    pad(date.getMonth(), 2)  + ":" +
+    pad(date.getDate(), 2) ;
+}
+function pad(num, size) {
+    num = num.toString();
+    while(num.length <size) {
+        num = "0" + num;  
+    }
+    return num;
+}
